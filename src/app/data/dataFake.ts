2fb92d6c20b876fb9e23cc186325081d53a71af7
jeanmends <jeanmenreis@hotmail.com>
2023-10-22 19:08:52 -0300

    final

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"HOMEM-ARANHA 4",
+    "description":"Novo filme do Homem-aranha anunciado",
+    "photoCover": "https://www.comboinfinito.com.br/principal/wp-content/uploads/2021/04/3-spider-men-1249301-1280x0-1.jpg"
+  },
+  {
+    "id":"2",
+    "title":"saiu novo filme",
+    "description":"ola",
+    "photoCover": "https://www.otempo.com.br/image/contentid/policy:1.3257040:1697670304/image-Teoria-maluca-de-Loki-quase-foi-usada-pelos-roteiristas.jpg"
+  },
+  {
+    "id":"3",
+    "title":"DAREDEVIL",
+    "description":"Saiba tudo sobre a nova temporada de Loki",
+    "photoCover": "https://criticalhits.com.br/wp-content/uploads/2023/01/demolidor.jpg"
+  },
+  {
+    "id":"4",
+    "title":"QUARTETO FANTÁSTICO",
+    "description":"Saiba tudo sobre a nova temporada de Loki",
+    "photoCover": "https://marvelucm.com/wp-content/uploads/2022/05/2020-05-20-OMELETV-DQ-1.jpg"
+  }
+]
